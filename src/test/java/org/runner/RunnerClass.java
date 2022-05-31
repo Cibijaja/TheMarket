5adb620df72093e6e97d42e38b422ebb9924c43c
Cibijaja <cibijaja@gmail.com>
2022-05-31 20:14:21 +0530

    android

--- src/test/java/org/runner/RunnerClass.java
+++ src/test/java/org/runner/RunnerClass.java
@@ -7,11 +7,14 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features= {"C:\\Users\\CIBI\\eclipse-workspace\\TheMarket\\src\\test\\resources\\FeatureFiles\\search.Feature"},
-				glue= {"org.stepDefinition","org.hooks"},dryRun=false,monochrome=true,tags="not @notNow",
+@CucumberOptions(features= {"C:\\Users\\CIBI\\eclipse-workspace\\TheMarket\\src\\test\\resources\\FeatureFiles\\search.Feature",
+		"C:\\Users\\CIBI\\eclipse-workspace\\TheMarket\\src\\test\\resources\\FeatureFiles\\Login.Feature"},
+				glue= {"org.stepDefinition","org.hooks"},dryRun=false,monochrome=true,
 				plugin= {"pretty",
 						"json:target\\MyReport\\report1.json",
-						"html:target\\\\MyReport\\\\report1.html"
+						"html:target\\\\MyReport\\\\report2.html",
+						"junit:target\\\\MyReport\\\\report3.xml",
+						
 })
 
 
